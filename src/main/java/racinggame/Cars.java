e8216b64dc2e200a790c3b6a1b1705d1a8baeef1
윤준석 <y2o2u2n@gmail.com>
2021-10-10 17:45:07 +0900

    refactor: 매직 스트링을 정적 변수로 옮김

--- src/main/java/racinggame/Cars.java
+++ src/main/java/racinggame/Cars.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+	private static final String DELIMITER = ",";
+
 	private final List<Car> values;
 
 	public Cars(String namesWithComma) {
 		List<Car> values = new ArrayList<>();
 
-		String[] names = namesWithComma.split(",");
+		String[] names = namesWithComma.split(DELIMITER);
 		for (String name : names) {
 			values.add(new Car(new Name(name)));
 		}
